a3a2204129fdfc67c2181de92e6fec4c612fb689
ksillah <kutah.sillah@gmail.com>
2021-05-21 20:57:38 -0500

    bare minimum

--- js/main.js
+++ js/main.js
@@ -1,42 +1,8 @@
-
-// Form submission
 let form = document.querySelector('#testDataForm')
 
-
-
-
-// let headings =['Position','Name','Nationality','Sponsor','Points']
-// function createTable2()
-// {
-//     // create table
-//     const table = document.createElement("table");
-
-//     // create caption and add to table
-//     const caption = document.createElement("caption");
-//     const captiontext = document.createTextNode("Elements");
-//     caption.appendChild(captiontext);
-//     table.appendChild(caption);
-
-//     // create row for headings...
-//     const hrow = document.createElement("tr");
-//     table.appendChild(hrow);
-
-//     // ...and add cells to it
-//     for(let heading of headings)
-//     {
-//         const th = document.createElement("th");
-//         const thtext = document.createTextNode(heading);
-//         th.appendChild(thtext);
-//         hrow.appendChild(th);
-//     }
-// }
-
-// racer example
-
 const racer_data = async (racers) => {
-    
+    racers=[]
     let response = await axios.get(`https://ergast.com/api/f1/${query_season}/${query_round}/driverStandings.json`)
-    //console.log(response.data)
     for(let i = 0;i<7;i++){
         let position=i+1;
         let firstName = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName;
@@ -46,19 +12,21 @@ const racer_data = async (racers) => {
         let sponsor= response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].constructorId;
         sponsor = sponsor.charAt(0).toUpperCase() + sponsor.slice(1);
         let points = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
-        let result = {position,name,nationality,sponsor,points}
+        let result = [position,name,nationality,sponsor,points]
+        let row = table.insertRow();
+        for (key in result) {
+          let cell = row.insertCell();
+          let text = document.createTextNode(result[key]);
+          cell.appendChild(text);
+        }
         racers.push(result);
-        //console.log(`racers : ${racers[i]}`)
-        //console.log(`racers : ${racers}`)}
-
-        //console.log(`Postion': ${position}, Name  ${firstName} ${lastName}, Nationality ${nationality}, sponsor${sponsor}, points${points}`)
     }
-    //console.log(`racers : ${racers}`)
     return racers
 }
 
 
 function generateTableHead(table) {
+    table.deleteTHead()
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of headings) {
@@ -67,36 +35,24 @@ function generateTableHead(table) {
         th.appendChild(text);
         row.appendChild(th);
       }
-    }
+}
 
-  let table = document.querySelector("table");
- 
-
-  function generateTable(table, racers) {
-    console.log('here')
-    for ( element of racers) {
-        console.log(element)
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-  }
-  // add event listener
+let table = document.querySelector("table");  
 let query_season
 let query_round
 let headings =['Position','Name','Nationality','Sponsor','Points']
+
 form.addEventListener('submit', (event) => {
+  for(var i = table.rows.length - 1; i > 0; i--)
+  {
+      table.deleteRow(i);
+  }
     event.preventDefault();
     query_season = document.querySelector('#season').value
     query_round = document.querySelector('#round').value
     const racers=[]
-    racer_data(racers)
-    console.log(racers)   
     generateTableHead(table);
-    generateTable(table,racers);
+    racer_data(racers)  ;
 })
 
   
